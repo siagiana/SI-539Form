c584d613d838322b7dd70db62c012c9755bf2faf
Niro Haryamen <niromrais@gmail.com>
2014-11-17 23:56:13 -0500

    changes made by niro
    1) move the error class to both formstyle and formstyle2 css
    2) finish onchange.js
    3) added several divs in html so that every label is better aligned

--- onchange.js
+++ onchange.js
@@ -9,17 +9,45 @@ This js file is developed by Niro Haryamen Mohd Rais with the help of Hasbullah 
 
 */
 
-function validateOnChange (form){
-	
-}
-
 $(document).ready(function() {
+	checkLastName('#last_name');
+	checkFirstName('#first_name');
 	checkEmail('#email_address');
 	sameEmail('#email_address','#email_confirm');
 	checkPassword('#password_requirement');
 	samePassword('#password_requirement', '#password');
-
+	checkNickname('#Nickname');
+	checkCity('#city');
+	checkZip('#zipcode');
 });
+
+function checkFirstName(FirstID){
+	$(FirstID).on('change', function(){
+		var FirstNameSelector = $(this);
+		var errorSelector = FirstNameSelector.siblings('.error');
+
+		if (FirstNameSelector.val() == ""){
+			errorSelector.text("").text("You did NOT enter your first name.");
+		}
+		else{
+			errorSelector.text("");
+		}
+	});
+}
+
+function checkLastName(LastID){
+	$(LastID).on('change', function(){
+		var LastNameSelector = $(this);
+		var errorSelector = LastNameSelector.siblings('.error');
+
+		if (LastNameSelector.val() == ""){
+			errorSelector.text("").text("You did NOT enter your last name.");
+		}
+		else{
+			errorSelector.text("");
+		}
+	});
+}
 
 function checkEmail(EmailID){
 	$(EmailID).on('change', function(){
@@ -37,7 +65,7 @@ function checkEmail(EmailID){
 		else{
 			errorSelector.text("");
 		}
-	})
+	});
 }
 
 function sameEmail(EmailId, EmailID2){
@@ -49,8 +77,8 @@ function sameEmail(EmailId, EmailID2){
 		var errorSelector1 = emailSelector1.siblings('.error');
 		var errorSelector2 = emailSelector2.siblings('.error');
 
-		console.log(emailSelector1.val());
-		console.log(emailSelector2.val());
+		// console.log(emailSelector1.val());
+		// console.log(emailSelector2.val());
 
 		if (errorSelector1.text()) {
 			errorSelector2.text("").text("Your email does not satisfy the requirements");
@@ -64,8 +92,6 @@ function sameEmail(EmailId, EmailID2){
 	});
 
 }
-
-
 
 function checkPassword(passwordId) {
 	$(passwordId).on('change', function() {
@@ -118,6 +144,73 @@ function samePassword(passwordId, passwordID2){
 	});
 
 }
+
+function checkNickname(NicknameId) {
+	$(NicknameId).on('change', function() {
+		var NicknameSelector = $(this);
+		// console.log(passwordSelector.val());
+		var errorSelector = NicknameSelector.siblings('.error');
+		//validate if not valid
+		if (NicknameSelector.val() == "") {
+			// console.log('1')
+			errorSelector.text("").text("No username was entered");
+		}
+		else if (NicknameSelector.val().length < 5) {
+			// console.log('2')
+			errorSelector.text("").text("Username must be at least 5 characters");
+		}
+		else if (/[^a-zA-Z0-9_-]/.test(NicknameSelector.val() )) {
+				// console.log('3')
+				errorSelector.text("").text("Username can only have a-z, A-Z and 0-9");
+		}
+		else {
+			// console.log('4')
+			errorSelector.text("");
+		}
+	});
+}
+
+function checkCity(CityId){
+	$(CityId).on('change', function(){
+		var CitySelector = $(this);
+		var errorSelector = CitySelector.siblings('.error');
+
+		if(CitySelector.val() == ""){
+			errorSelector.text("").text("No city was entered");
+		}
+		else{
+			errorSelector.text("");
+		}
+
+	});
+}
+
+function checkZip(ZipId) {
+	$(ZipId).on('change', function() {
+		var ZipSelector = $(this);
+		// console.log(passwordSelector.val());
+		var errorSelector = ZipSelector.siblings('.error');
+		//validate if not valid
+		if (ZipSelector.val() == "") {
+			// console.log('1')
+			errorSelector.text("").text("No zipcode was entered");
+		}
+		else if (ZipSelector.val().length != 5) {
+			// console.log('2')
+			errorSelector.text("").text("Zipcode must have 5 characters");
+		}
+		else if (/[^0-9_-]/.test(ZipSelector.val() )) {
+				// console.log('3')
+				errorSelector.text("").text("Zipcode can only have number 0-9");
+		}
+		else {
+			// console.log('4')
+			errorSelector.text("");
+		}
+	});
+}
+
+
 
 
 
